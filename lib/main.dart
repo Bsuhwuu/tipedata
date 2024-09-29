ebfa3c70b6e1edb7b2083e260dc7487c9a4308f5
Bsuhwuu <ridhonovan951@gmail.com>
2024-09-29 16:02:06 +0700

    center-undo

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
-        centerTitle: true,
+        
       ),
       body: Center(
         child: Column(
